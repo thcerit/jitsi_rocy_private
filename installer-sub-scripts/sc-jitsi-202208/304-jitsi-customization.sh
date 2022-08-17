89cabc2c8a867ddb23411fe23908dfb243fd8f7b
emrah <emrah.com@gmail.com>
2022-08-17 12:36:24 +0300

    sc-jitsi-202208, refactor: create a new customization folder if there is already one

--- installer-sub-scripts/sc-jitsi-202208/304-jitsi-customization.sh
+++ installer-sub-scripts/sc-jitsi-202208/304-jitsi-customization.sh
@@ -23,25 +23,23 @@ echo "------------------- JITSI CUSTOMIZATION -------------------"
 # ------------------------------------------------------------------------------
 JITSI_ROOTFS="/var/lib/lxc/eb-jitsi/rootfs"
 JITSI_MEET="$JITSI_ROOTFS/usr/share/jitsi-meet"
+FOLDER="/root/jitsi-customization"
 
-if [[ ! -d "/root/jitsi-customization" ]]; then
-    cp -arp root/jitsi-customization-202208 /root/jitsi-customization
+# is there an old customization folder?
+if [[ -d "/root/jitsi-customization" ]]; then
+    FOLDER="/root/jitsi-customization-new"
+    rm -rf $FOLDER
 
-    sed -i "s/___TURN_FQDN___/$TURN_FQDN/g" \
-        /root/jitsi-customization/README.md
-    sed -i "s/___JITSI_FQDN___/$JITSI_FQDN/g" \
-        /root/jitsi-customization/README.md
-    sed -i "s/___TURN_FQDN___/$TURN_FQDN/g" \
-        /root/jitsi-customization/customize.sh
-    sed -i "s/___JITSI_FQDN___/$JITSI_FQDN/g" \
-        /root/jitsi-customization/customize.sh
-
-    cp $JITSI_ROOTFS/etc/jitsi/meet/$JITSI_FQDN-config.js \
-        /root/jitsi-customization/files/
-    cp $JITSI_ROOTFS//usr/share/jitsi-meet/interface_config.js \
-        /root/jitsi-customization/files/
-else
     echo "There is already an old customization folder."
-    echo "Automatic customization skipped."
-    echo "Run your own customization script manually."
+    echo "A new folder will be created as $FOLDER"
 fi
+
+cp -arp root/jitsi-customization-202208 $FOLDER
+
+sed -i "s/___TURN_FQDN___/$TURN_FQDN/g" $FOLDER/README.md
+sed -i "s/___JITSI_FQDN___/$JITSI_FQDN/g" $FOLDER/README.md
+sed -i "s/___TURN_FQDN___/$TURN_FQDN/g" $FOLDER/customize.sh
+sed -i "s/___JITSI_FQDN___/$JITSI_FQDN/g" $FOLDER/customize.sh
+
+cp $JITSI_ROOTFS/etc/jitsi/meet/$JITSI_FQDN-config.js $FOLDER/files/
+cp $JITSI_ROOTFS//usr/share/jitsi-meet/interface_config.js $FOLDER/files/
