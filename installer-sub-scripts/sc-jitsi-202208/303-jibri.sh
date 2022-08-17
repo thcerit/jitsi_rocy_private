381acd68a05c26ac8218f064f2b672986183b79c
emrah <emrah.com@gmail.com>
2022-08-17 20:42:53 +0300

    sc-jitsi-202208, refactor: backup original config filesi for jibri

--- installer-sub-scripts/sc-jitsi-202208/303-jibri.sh
+++ installer-sub-scripts/sc-jitsi-202208/303-jibri.sh
@@ -20,7 +20,7 @@ echo
 echo "-------------------------- $MACH --------------------------"
 
 # ------------------------------------------------------------------------------
-# JITSI CUSTOMIZATION FOR JIBRI
+# PROSODY
 # ------------------------------------------------------------------------------
 # prosody config
 cp $MACHINES/eb-jitsi/etc/prosody/conf.avail/recorder.cfg.lua \
@@ -53,7 +53,9 @@ echo $PASSWD1 > /root/meta/jibri-passwd
 echo $PASSWD2 > /root/meta/recorder-passwd
 EOS
 
-# jicofo config
+# ------------------------------------------------------------------------------
+# JICOFO
+# ------------------------------------------------------------------------------
 lxc-attach -n eb-jitsi -- zsh <<EOS
 set -e
 hocon -f /etc/jitsi/jicofo/jicofo.conf \
