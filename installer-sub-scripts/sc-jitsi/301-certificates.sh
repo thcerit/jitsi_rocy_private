c53657bf5fc3ecd4c6695dfe63bd26ffdc995e06
serdar28 <44525073+serdar28@users.noreply.github.com>
2023-01-13 15:12:10 +0300

    Update 301-certificates.sh

--- installer-sub-scripts/sc-jitsi/301-certificates.sh
+++ installer-sub-scripts/sc-jitsi/301-certificates.sh
@@ -20,7 +20,8 @@ echo "---------------------- CERTIFICATES -----------------------"
 # ------------------------------------------------------------------------------
 # EXTERNAL IP
 # ------------------------------------------------------------------------------
-EXTERNAL_IP=$(dig -4 +short myip.opendns.com a @resolver1.opendns.com) || true
+#EXTERNAL_IP=$(dig -4 +short myip.opendns.com a @resolver1.opendns.com) || true
+EXTERNAL_IP="185.118.193.61"
 echo EXTERNAL_IP="$EXTERNAL_IP" >> $INSTALLER/000-source
 
 # ------------------------------------------------------------------------------
