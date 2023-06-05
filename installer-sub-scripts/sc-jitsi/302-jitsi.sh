a4fde0c9584d21db9dd376410af9b799454b4805
serdar28 <44525073+serdar28@users.noreply.github.com>
2023-06-05 11:00:39 +0300

    Update 302-jitsi.sh

--- installer-sub-scripts/sc-jitsi/302-jitsi.sh
+++ installer-sub-scripts/sc-jitsi/302-jitsi.sh
@@ -218,7 +218,8 @@ EOS
 # ------------------------------------------------------------------------------
 # EXTERNAL IP
 # ------------------------------------------------------------------------------
-EXTERNAL_IP=$(dig -4 +short myip.opendns.com a @resolver1.opendns.com) || true
+#EXTERNAL_IP=$(dig -4 +short myip.opendns.com a @resolver1.opendns.com) || true
+EXTERNAL_IP="185.118.193.52"
 echo EXTERNAL_IP="$EXTERNAL_IP" >> $INSTALLER/000-source
 
 # ------------------------------------------------------------------------------
