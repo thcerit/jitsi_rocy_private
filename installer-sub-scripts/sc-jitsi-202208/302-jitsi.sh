81095c0b36501cefb4d3e7f066f9b5691ec8fd9e
enesdoganoglu46 <137179234+enesdoganoglu46@users.noreply.github.com>
2024-07-08 11:44:43 +0300

    Update 302-jitsi.sh

--- installer-sub-scripts/sc-jitsi-202208/302-jitsi.sh
+++ installer-sub-scripts/sc-jitsi-202208/302-jitsi.sh
@@ -410,13 +410,17 @@ cp $ROOTFS/etc/jitsi/videobridge/jvb.conf \
     $ROOTFS/etc/jitsi/videobridge/jvb.conf.org
 cp $ROOTFS/etc/jitsi/videobridge/sip-communicator.properties \
     $ROOTFS/etc/jitsi/videobridge/sip-communicator.properties.org
-JVB_SHARD_PASSWD=$(egrep '^org.jitsi.videobridge.xmpp.user.shard.PASSWORD=' \
-    $ROOTFS/etc/jitsi/videobridge/sip-communicator.properties | \
-    cut -d '=' -f2)
 # meta
 lxc-attach -n $MACH -- zsh <<EOS
 set -e
 mkdir -p /root/meta
+chmod 700 /root/meta
+echo $JITSI_FQDN >/root/meta/jitsi-fqdn
+EOS
+JVB_SHARD_PASSWD=$(egrep '^org.jitsi.videobridge.xmpp.user.shard.PASSWORD=' \
+    $ROOTFS/etc/jitsi/videobridge/sip-communicator.properties | \
+    cut -d '=' -f2)
+lxc-attach -n $MACH -- zsh <<EOS
 echo '$JVB_SHARD_PASSWD' >/root/meta/jvb-shard-passwd
 chmod 600 /root/meta/jvb-shard-passwd
 VERSION=\$(apt-cache policy jitsi-videobridge2 | grep Installed | rev | \
