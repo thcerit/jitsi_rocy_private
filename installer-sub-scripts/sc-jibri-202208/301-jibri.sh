3a97428687bd8d1f04a9b3c7a5d38c2eb609ad64
thcerit <36306750+thcerit@users.noreply.github.com>
2024-09-26 13:49:03 +0300

    Update 301-jibri.sh

--- installer-sub-scripts/sc-jibri-202208/301-jibri.sh
+++ installer-sub-scripts/sc-jibri-202208/301-jibri.sh
@@ -350,23 +350,6 @@ chmod 744 /usr/local/sbin/jibri-ephemeral-stop
 cp $MACHINES/eb-jibri-host/etc/systemd/system/jibri-ephemeral-container.service \
     /etc/systemd/system/
 
-#systemctl daemon-reload
-#systemctl enable jibri-ephemeral-container.service
-#systemctl start jibri-ephemeral-container.service
-#/usr/local/sbin/jibri-ephemeral-start
-
-lxc-copy -n eb-jibri-template -N eb-jibri-1 -e
-lxc-copy -n eb-jibri-template -N eb-jibri-2 -e
-lxc-copy -n eb-jibri-template -N eb-jibri-3 -e
-echo "-----------------------------------------"
-lxc-ls -f
-echo "-----------------------------------------"
-echo "Starting containers.."
-lxc-start --name eb-jibri-1
-sleep 5
-lxc-start --name eb-jibri-2
-sleep 5
-lxc-start --name eb-jibri-3
-echo "-----------------------------------------"
-lxc-ls -f
-echo "-----------------------------------------"
+systemctl daemon-reload
+systemctl enable jibri-ephemeral-container.service
+systemctl start jibri-ephemeral-container.service
